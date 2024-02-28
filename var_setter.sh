c1385ecc13bd5103bf59d9038a566769044f0db5
Ahmed Said <ahmed.el.sayeed1914@gmail.com>
2024-02-28 15:48:21 +0200

    added archive and restore functionality for exam scripts

--- var_setter.sh
+++ var_setter.sh
@@ -54,77 +54,163 @@ set_hosts() {
 		set_host $itr
 	done
 }
-SERVER_COUNT="$(ls -d environments/server_* 2> /dev/null| wc -l)"
-if [[ $SERVER_COUNT == 0 ]]
-then
-	set_hosts
-else
-	echo "There is already $SERVER_COUNT preset server directories, skipping directory creation."
-fi
-#echo "these are the variables and their defaults values"
- OPTS="set add remove list clear quit"
- select opt in $OPTS
- do
- 	echo "you chose ($REPLY)$opt"
- 	case $opt in
-		"set")
-			echo "1)set the current host for these problems"
-			ls -d environments/server_* &> /dev/null || set_host 1
-			select opt in $(ls environments/*/ -d) "add host" "quit"
-			do
-				case $opt in
-					"add host")
-						let count=$(ls environments/*/ -d | wc -l)+1
-						HOST=$(set_host $count)
-						;;
-					"quit")
-						break
-						;;
-					*)
-						echo "host $opt is now selected, please choose problems for this host"
-						HOST="$opt"
-						break
-						;;
-				esac
-			done
-			;;
- 		"add")
- 			echo "2)set variables to the practice environment"
-			select dir in problems scripts
-			do
-				echo "you can exit by selecting q"
-	 			select script in $(ls $dir/)
-	 			do
-					[[ $REPLY == "q" ]] && break
-	 				echo "$script is now selected"
-	 				get_vars $dir $script
-	 			done
-				break
-			done
-			;;
- 		"remove")
- 			echo "3)remove a problem"
-			select problem in $(ls $HOST)
- 			do
- 				rm "$HOST/$problem"
- 				break
- 			done
- 			;;
- 		"list")
- 			echo "4) show selected hosts problem variables"
-				ls $HOST/{problems,scripts}
- 			;;
-		"clear")
-			echo "5) clear the already existing problems"
+get_hosts(){
+	count=$(ls -d environments/server_* 2> /dev/null| wc -l)
+	echo $count
+}
+clear_env() {	
+	reply=$1
+	[[ -n $reply ]] || read -p "Are you sure you want to delete the current environment configuration? (yes/no): " reply
+	case $reply in 
+		"y"|"yes"|"Y"|"YES")
 			rm -rf environments/server_*
 			echo "" > environments/hosts.env
-			echo "cleared..."
+			echo "Environment configurations cleared..."
 			;;
- 		"help")
- 			;;
- 		"quit")
- 			echo "exiting..."
- 			exit 0
- 			;;
- 	esac
- done
+		"n"|"no"|"N"|"NO")
+			;;
+	esac
+}
+archive_exam(){
+	read -p "archive name: " name
+	tar cf "archive/$name-$(date "+%Y-%m-%d")-archive.tar" environments/hosts.env environments/server_*
+}
+restore_exam(){
+	select opt in $(ls archive)
+	do
+		tar xf archive/$opt
+		break
+	done
+}
+interactive () {
+	SERVER_COUNT=$(get_hosts)
+	OPTS="set add remove list archive restore clear quit"
+	if [[ $SERVER_COUNT == 0 ]]
+	then
+		set_hosts
+	else
+		echo "There is already $SERVER_COUNT preset server directories, skipping directory creation."
+	fi
+	#echo "these are the variables and their defaults values"
+	select opt in $OPTS
+	 do
+	 	echo "you chose ($REPLY)$opt"
+	 	case $opt in
+			"set")
+				echo "1)set the current host for these problems"
+				echo "There are currently $(get_hosts) hosts setup"
+#				(( $(get_hosts) )) || set_host 1
+				select opt in $(ls environments/*/ -d 2> /dev/null) "add host" "quit"
+				do
+					case $opt in
+						"add host")
+							let count=$(get_hosts)+1
+							HOST=$(set_host $count)
+							break
+							;;
+						"quit")
+							break
+							;;
+						*)
+							echo "host $opt is now selected, please choose problems for this host"
+							HOST="$opt"
+							break
+							;;
+					esac
+				done
+				;;
+	 		"add")
+	 			echo "2)set variables to the practice environment"
+				select dir in problems scripts
+				do
+					echo "you can exit by selecting q"
+		 			select script in $(ls $dir/)
+		 			do
+						[[ $REPLY == "q" ]] && break
+		 				echo "$script is now selected"
+		 				get_vars $dir $script
+		 			done
+					break
+				done
+				;;
+	 		"remove")
+	 			echo "3)remove a problem"
+				select problem in $(ls $HOST)
+	 			do
+	 				rm "$HOST/$problem"
+	 				break
+	 			done
+	 			;;
+	 		"list")
+	 			echo "4) show selected hosts problem variables"
+					ls $HOST/{problems,scripts}
+	 			;;
+			"archive")
+				echo ")archive the current exam with all its parameters and variables"
+				archive_exam
+				;;
+			"restore")
+				echo ") restores an exam with all its data"
+				let count=$(get_hosts)
+				if (( count )) 
+				then
+					echo "There is already $count server directories set."
+					read -p "Do you want to archive them(yes/no)? >> " reply
+					case $reply in 
+						"y"|"yes"|"Y"|"YES")
+							archive_exam
+							echo "exam archived, deleting current configs..."
+							clear_env $reply
+							;;
+						"n"|"no"|"N"|"NO")
+							clear_env
+							;;
+					esac
+				fi
+				restore_exam
+				;;
+			"clear")
+				echo "5) clear the already existing problems"
+				clear_env
+				;;
+	 		"help")
+	 			;;
+	 		"quit")
+	 			echo "exiting..."
+	 			exit 0
+	 			;;
+	 	esac
+	 done
+ }
+#short_options="s:a:r:m:l:c:h"
+#long_options=("select:" "add:" "remove:" "modify:" "list:" "clear:" "help:")
+#
+# while getopts "$short_options" opt;
+# do
+#	 case $opt in
+#		 s)
+#			 let count=$get_hosts+1
+#			 HOST="environments/server_$count/"
+#			 mkdir -p $HOST/{scripts,problems}
+#			 ;;
+#		 a)
+#			 echo "a"
+#			 ;;
+#		 r)
+#			 echo "r"
+#			 ;;
+#		 m)
+#			 echo "m"
+#			 ;;
+#		 l)
+#			 echo "l"
+#			 ;;
+#		 c)
+#			 echo "c"
+#			 ;;
+#		 h)
+#			 echo "h"
+#			 ;;
+#	esac
+# done
+interactive
