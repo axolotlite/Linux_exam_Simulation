15d60bd732a0c870240e71a4281f3acb4cde9ec7
Ahmed Said <ahmed.el.sayeed1914@gmail.com>
2024-02-29 20:20:09 +0200

    refactored remote.sh and added some more functionality

--- remote.sh
+++ remote.sh
@@ -9,6 +9,8 @@ USER="root"
 PUBKEY="credentials/utility.pub"
 PRIKEY="credentials/utility"
 DEBUG=false
+FUNC_EXEC=true
+CCOUNT=true
 let count=1
 transfer_credentials() {
 	address="${!CURRENT_ADDRESS}"
@@ -20,6 +22,18 @@ transfer_credentials() {
 		ssh -i $PRIKEY $USER@$address "update-ca-trust"
 	fi
 }
+execute_script() {
+	filename=$1
+	address=$2
+	directory=${filename%/*}
+	directory=${directory##*/}
+	filename=${filename##*/}
+	filename=${filename%.*}
+	varfile="environments/server_$count/$directory/$filename.vars"
+	scriptfile="$directory/$filename.sh"
+	#echo -e "directory: $directory\nfilename: $filename\nvarfile: $varfile\nscriptfile: $scriptfile\n----"
+	cat "$varfile" "$scriptfile" | ssh -i $PRIKEY "$USER@${!CURRENT_ADDRESS}" "bash -s "
+}
 setup_host() {
 	number="$count"
 	var_dir="environments/server_$number/scripts"
@@ -27,10 +41,11 @@ setup_host() {
 	echo "setting up scripts for user $USER at $adddress..."
 	for varfile in $(ls $var_dir)
 	do
-		script="scripts/${varfile%.*}.sh"
+		#script="scripts/${varfile%.*}.sh"
 		[[ $DEBUG == "true" ]] && read -p "press enter to continue executing $script... " empy
-		echo "executing $script"
-		cat "$var_dir/$varfile" "$script" | ssh -i $PRIKEY $USER@$address "bash -s "
+		echo "executing ${varfile#*/}"
+#		cat "$var_dir/$varfile" "$script" | ssh -i $PRIKEY $USER@$address "bash -s "
+		execute_script "$var_dir/$varfile" 
 		#echo $varfile:$script
 	done
 }
@@ -41,10 +56,11 @@ grade_host() {
 	echo "grading ${!CURRENT_HOSTNAME}..."
 	for varfile in $(ls $var_dir)
 	do
-		script="problems/${varfile%.*}.sh"
-		echo "executing $script..." 
+		#script="problems/${varfile%.*}.sh"
+		echo "executing ${varfile#*/}"
 		[[ $DEBUG == "true" ]] && read -p "press enter to continue executing $script... " empy
-		cat "$var_dir/$varfile" "$script" | ssh -i $PRIKEY $USER@$address "bash -s "
+		#cat "$var_dir/$varfile" "$script" | ssh -i $PRIKEY $USER@$address "bash -s "
+		execute_script "$var_dir/$varfile"
 	done
 }
 help_func() {
@@ -53,10 +69,12 @@ help_func() {
 	echo "-t		transfer credentials to the user directories"
 	echo "-s		execute the setup scripts in each host"
 	echo "-g		execute the grading scripts and print the output"
+	echo "-c		the number of the server you want to setup"
 	echo "-v		verbose, is still in implementation"
+	echo "-f		file.sh to execute on a selected server"
 	echo "-h		print the usage/help text, like this"
 }
-while getopts "tsghv" opt
+while getopts "tsf:ghvc:" opt
 do
 	case $opt in
 		t)
@@ -68,8 +86,24 @@ do
 		g)
 			opt_func=grade_host
 			;;
+		c)
+			let count=${OPTARG}
+			let servers=$(ls -d environments/server_*/| wc -l)
+			if (( count < 0 )) || (( count > servers ))
+			then
+				echo "The selected Server doesn't exist, please choose a server between 1 and $servers"
+				exit 1
+			fi
+			CCOUNT=false
+			;;
 		v)
 			DEBUG=true
+			;;
+		f)
+
+			script=${OPTARG}
+			execute_script "$script"
+			FUNC_EXEC=false
 			;;
 		h)
 
@@ -83,7 +117,7 @@ do
 
 
 done
-while [[ -n ${!CURRENT_HOSTNAME} ]]
+while [[ -n ${!CURRENT_HOSTNAME} ]] && [[ $FUNC_EXEC == "true" ]]
 do
 #	echo "$CURRENT_HOSTNAME:${!CURRENT_HOSTNAME}"
 #	echo "$CURRENT_ADDRESS:${!CURRENT_ADDRESS}"
@@ -91,7 +125,7 @@ do
 	#transfer_credentials ${!CURRENT_ADDRESS}
 	#setup_host $count ${!CURRENT_ADDRESS}
 	$opt_func
-	((count++))
+	[[ $CCOUNT=="true" ]] && ((count++))
 	CURRENT_HOSTNAME="HOSTNAME_$count"
 	CURRENT_HOST="HOST_$count"
 	CURRENT_ADDRESS="ADDRESS_$count"
